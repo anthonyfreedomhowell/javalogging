16dfc3b21fa282204ca781f0e9b32ba6a70d7c88
Anthony Freedom Howell <anthonyfreedomhowell@gmail.com>
2019-02-17 01:25:41 -0500

    Created src/ directory and placed JavaLogging.java in there.

---
+++ src/JavaLogging.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.*;
+
+///This program logs what the user wants to log.
+//first the program will log the user's name, 
+//the date in a mm-dd-yyyy format and finally
+//how many javas the user has had.
+
+
+public class JavaLogging {
+
+	public static void main(String[] args) {
+	 System.out.println("Welcome to the Java logging program. This program logs " +
+			"how many javas you have had on a particular day.");
+
+
+	}
+
+
+
+}
